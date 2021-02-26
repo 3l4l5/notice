ada57a9ac8333adbdcf935de6fe96f1f1f89d6af
Ohkura Ryusei <ohkura@SpecteenoMacBook-Air.local>
2021-02-27 01:07:53 +0900

    最終調整

--- script/main.py
+++ script/main.py
@@ -4,6 +4,3 @@ from notify import send_message
 
 url = "https://condition.nihon-u.ac.jp/Student"
 send_message("\nもう熱測りましたか？？？\nまだ熱測ってない人は測って記録してね！\n"+url)
-
-if __name__ == "__main__":
-    send_message("今日から午前10時と午後６時に熱を測るよう自動で通知します！\nみんなしっかりと熱を測って卒業式に出ましょう！\n"+url)
